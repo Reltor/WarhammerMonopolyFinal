b6a17fb5da1e2f7846754c69ada5bf24bbb4086c
Subsight <38530980+Subsight@users.noreply.github.com>
2018-12-11 15:01:30 -0500

    IT CAN ADD!!!!!!!

--- JavaFinalProject/src/Controller/BattleTechIO.java
+++ JavaFinalProject/src/Controller/BattleTechIO.java
@@ -182,9 +182,68 @@ public class BattleTechIO {
         }
     }
     
-    public static void wipeFile()
+    public static void add(BattleMech bm)
     {
-        
+        Path fp = Paths.get("MechDB.txt");
+        File readFile = fp.toFile();
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<BattleMech> bmList = new ArrayList<BattleMech>();
+        try 
+        {
+
+            BufferedReader in = new BufferedReader(
+                                new FileReader(readFile));
+            String line = "";
+            while (line != null)
+                {
+                    int i = 0;
+                    String[] result = (line.split("\n"));
+                    list.add(i, result[i]);
+                    line = in.readLine();
+                    i++;
+                }
+            in.close();
+        }   
+        catch (IOException ex) 
+        {
+            Logger.getLogger(BattleTechIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (int i = 0;i < list.size()-1; i++)
+        {
+            String r = list.get(i);
+            String mech[] = (r.split(","));
+            String name = mech[0];
+            int ton = Integer.parseInt(mech[1]);
+            String type = mech[2];
+            int era = Integer.parseInt(mech[3]);
+            BattleMech bml = new BattleMech(name,ton,type,era);
+            bmList.add(bml);
+        }
+        bmList.add(bm);
+        Path fPath = Paths.get("MechDB.txt");
+        File outFile = fPath.toFile();
+        try 
+        {
+            PrintWriter w = new PrintWriter(
+                            new BufferedWriter(
+                            new FileWriter(outFile)));
+            
+            for (int i = 0; i < bmList.size();i++)
+            {
+                String line = (bmList.get(i).getName() + "," + bmList.get(i).getTonnage()+ "," + bmList.get(i).getClassName() + "," + bmList.get(i).getEra());
+                w.println(line);
+            }
+            w.close();
+            
+        }
+        catch (FileNotFoundException ex) 
+        {
+            Logger.getLogger(BattleTechIO.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+        catch (IOException ex) 
+        {
+            Logger.getLogger(BattleTechIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public static void editLine(BattleMech b)
