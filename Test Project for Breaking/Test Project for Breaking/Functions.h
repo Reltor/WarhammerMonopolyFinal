670af3c3377189b0580d313cd33e17e54c520c12
Subsight <38530980+Subsight@users.noreply.github.com>
2018-12-13 15:58:00 -0500

    Here you are good sir

--- Test Project for Breaking/Test Project for Breaking/Functions.h
+++ Test Project for Breaking/Test Project for Breaking/Functions.h
@@ -10,11 +10,13 @@
 #include <vector>
 #include "Space.h"
 #include "Player.h"
+#include "Properties.h"
 
 class Functions
 {
 public:
 	static void payRent(Player& p, Space& s);
 	static void setOwnerShip(Player& p, Space& s);
+	static void buyHouse(Player& p, Properties& s);
 
 };
