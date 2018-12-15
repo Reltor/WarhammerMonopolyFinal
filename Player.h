47e4dded90ff35cb86eac82b8b18b298c8de7a1d
Subsight <38530980+Subsight@users.noreply.github.com>
2018-12-15 15:50:16 -0500

    TradeFunction PlayerH PlayerCPP

---
+++ Player.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <random>
+#include <array>
+#include <vector>
+#include "Space.h"
+#//include "Properties.h"
+
+class Player
+{
+public:
+	Player();
+	void setLocation(int);
+	void setMoney(int);
+	void payRent(int);
+	void addMoney(int);
+	std::string getName();
+	int getMoney();
+
+
+protected:
+	int money;
+	int remain;
+	int location;
+	std::string name;
+	void setProperties(Space*);
+	std::vector<Space*> ownedProperties;
+};
+
